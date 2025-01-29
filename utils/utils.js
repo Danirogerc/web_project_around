0759b9d09aec4d7adebb57d1911c1f4a93db28a1
danirogerc <danirogerc@gmail.com>
2025-01-29 13:36:27 +0100

    feature-window finished

--- utils/utils.js
+++ utils/utils.js
@@ -4,16 +4,44 @@ import Card from "../blocks/card/cards.js";
 //Functions to open and close popups
 export function openPopup(popup) {
   popup.classList.remove("popup_hidden");
+  // Add event listeners when opening popup
+  document.addEventListener("keydown", handleEscClose);
+  document.addEventListener("mousedown", handleOverlayClick);
 }
+
 export function closePopup(popup) {
   popup.classList.add("popup_hidden");
+  // Remove event listeners when closing popup
+  document.removeEventListener("keydown", handleEscClose);
+  document.removeEventListener("mousedown", handleOverlayClick);
 }
+
+// Function to handle Esc key press
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup:not(.popup_hidden)");
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
+
+// Function to handle clicking outside popup
+function handleOverlayClick(evt) {
+  if (evt.target.classList.contains("popup__overlay")) {
+    const popup = evt.target.closest(".popup");
+    if (popup) {
+      closePopup(popup);
+    }
+  }
+}
+
 //Function to render one card
 export function renderCard(
   cardData,
   containerSelector,
   templateSelector,
-  handleDeleteCard,
+  handleDeleteCard
 ) {
   const container = document.querySelector(containerSelector);
   const card = new Card(cardData, templateSelector, handleDeleteCard);
@@ -27,9 +55,9 @@ export function renderInitialCards(
   initialCards,
   containerSelector,
   templateSelector,
-  handleDeleteCard,
+  handleDeleteCard
 ) {
   initialCards.forEach((cardData) =>
-    renderCard(cardData, containerSelector, templateSelector, handleDeleteCard),
+    renderCard(cardData, containerSelector, templateSelector, handleDeleteCard)
   );
 }
